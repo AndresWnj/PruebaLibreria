8576b254495d5f88d212916646e8684aff0d81de
AndresWnj <andres.wnj@gmail.com>
2016-11-21 15:36:32 -0500

    First Commit

---
+++ app/src/main/java/andres/wnj/pruebalibreria/Util/PruebaUtil.java
@@ -0,0 +1,13 @@
+package andres.wnj.pruebalibreria.Util;
+
+/**
+ * Created by Buxtar on 21/11/2016.
+ */
+
+public class PruebaUtil {
+    public static final int CODE_UTIL=2832;
+
+    public int getObject(){
+        return CODE_UTIL;
+    }
+}
